e4887ea42675e5f75c77a8a92d8079a362aacb14
jeongyw12382 <jeongyw12382@postech.ac.kr>
2021-12-14 01:15:27 +0900

    hotfix: config change

--- scripts/jaxnerf.sh
+++ scripts/jaxnerf.sh
@@ -2,17 +2,16 @@
 # LLFF
 python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/fern \
     --expname fern --train --eval
-python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/flower \
-    --expname flower --train --eval
-python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/fortress \
-    --expname fortress --train --eval
 python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/horns \
     --expname horns --train --eval
-python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/leaves \
-    --expname leaves --train --eval
-python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/orchids \
-    --expname orchids --train --eval
 python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/room \
     --expname room --train --eval
-python3 -m run --config configs/jaxnerf/llff.yaml --datadir data/llff/trex \
-    --expname trex --train --eval
+
+# BLENDER
+python3 -m run --config configs/jaxnerf/blender.yaml --datadir data/blender/chair \
+    --expname chair --train --eval
+python3 -m run --config configs/jaxnerf/blender.yaml --datadir data/blender/lego \
+    --expname lego --train --eval
+python3 -m run --config configs/jaxnerf/blender.yaml --datadir data/blender/ship \
+    --expname ship --train --eval
+    
