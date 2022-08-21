03abefbc7d01841341b503937b228ef873c93c74
Junha Lee <junhakiwi@postech.ac.kr>
2022-08-21 23:42:29 +0900

    minor fix

--- utils/extract_stats.py
+++ utils/extract_stats.py
@@ -42,9 +42,9 @@ if __name__ == "__main__":
         metrics = metrics[~np.isnan(metrics)]
         if name == "psnr":
             print(
-                f"avg {name:>5} = {np.mean(metrics):.3f}, > 15 = {np.mean(metrics > 15):.3f}, > 20 = {np.mean(metrics > 20):.3f}, > 25 = {np.mean(metrics > 25):.3f}, max = {np.max(metrics):.3f}, min = {np.min(metrics):.3f}"
+                    f"{name:>5}: {np.mean(metrics):.3f}+-{np.std(metrics):.3f}, > 15: {np.mean(metrics > 15):.3f}, > 20: {np.mean(metrics > 20):.3f}, > 25: {np.mean(metrics > 25):.3f}, max: {np.max(metrics):.3f}, min: {np.min(metrics):.3f}, 25th percentile: {np.percentile(metrics, 25):.3f}, 50th percentile: {np.percentile(metrics, 50):.3f}, 75th percentile: {np.percentile(metrics, 75):.3f}, 90th: {np.percentile(metrics, 90):.3f}, 95th: {np.percentile(metrics, 95):.3f}"
             )
         else:
             print(
-                f"avg {name:>5} = {np.mean(metrics):.3f}, max = {np.max(metrics):.3f}, min = {np.min(metrics):.3f}"
+                f"avg {name:>5}: {np.mean(metrics):.3f}, max: {np.max(metrics):.3f}, min: {np.min(metrics):.3f}"
             )
